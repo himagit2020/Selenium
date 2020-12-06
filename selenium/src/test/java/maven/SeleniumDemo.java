9fd481876db430ccdb9275006bcd46fe0b0bb25e
himagit2020 <hima.bindu.kothapalli@gmail.com>
2020-12-06 07:49:32 -0600

    code pushed

--- selenium/src/test/java/maven/SeleniumDemo.java
+++ selenium/src/test/java/maven/SeleniumDemo.java
@@ -4,6 +4,7 @@ public class SeleniumDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Program execution is started !!!");
+		System.out.println("Himapushed the code to Git");
 
 	}
 
